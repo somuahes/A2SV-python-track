eccbd32baf011e6258033f422a28a350813cf900
somuahes <154274014+somuahes@users.noreply.github.com>
2025-03-13 12:08:05 +0000

    Update runnerup.py

--- runnerup.py
+++ runnerup.py
@@ -1,10 +1,8 @@
 if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    score=list(arr)
-    winner=max(score)
-    while winner in score:
-        score.remove(winner)
-    runner_up=max(score)
+    n=int(input())
+    arr=map(int,input().split())
+    scores=set(arr)
+    sort_scores=sorted(scores)
+    runner_up=sort_scores[-2]
     print(runner_up)
     
