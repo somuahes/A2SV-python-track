8d87bf3ccb1709a3e56417eeb66d8e8b3fa8416b
somuahes <154274014+somuahes@users.noreply.github.com>
2025-03-13 15:58:26 +0000

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,4 +1,19 @@
 def swap_case(s):
-    return s.swapcase()
-
+    result=""
+    for char in s:
+        if char.isupper():
+            result+=char.lower()
+        elif char.islower():
+            result+=char.upper()
+        else :
+            result+=char
+    return result     
+       
+    
+        
+        
+       
 if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
