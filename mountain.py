ede8aabf57c970d0ada549854e11ceb9ecd5a906
somuahes <154274014+somuahes@users.noreply.github.com>
2025-04-08 07:07:16 +0000

    Add files via upload

---
+++ mountain.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def validMountainArray(self, arr):
+        if len(arr) < 3:
+            return False
+        i = 0
+        while i + 1 < len(arr) and arr[i] < arr[i+1]:
+            i += 1
+        if i == 0 or i == len(arr)-1:
+            return False
+
+        while i+1 < len(arr) and arr[i] > arr[i+1]:
+            i += 1
+        if i == len(arr)-1:
+            return True
